12b6cd7fc58498e02f2025a28adfe58f4e6ecd62
xiuye <xiuye_engineer@outlook.com>
2019-05-29 14:45:19 +0800

    new project

---
+++ src/main/java/com/xy/xy/XyApplication.java
@@ -0,0 +1,13 @@
+package com.xy.xy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyApplication.class, args);
+	}
+
+}
